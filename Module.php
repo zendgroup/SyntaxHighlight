c81d1e6f6702dc3c1a1a942faccc437feceecf83
Thuy Dinh Xuan <thuydx@zendgroup.vn>
2014-03-06 02:34:23 +0700

    Update license
    Signed-off-by: Thuy Dinh Xuan <thuydx@zendgroup.vn>

--- Module.php
+++ Module.php
@@ -1,9 +1,27 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
+ * ZEND GROUP
  *
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @name        Module.php
+ * @category
+ * @package     SyntaxHighlight
+ * @subpackage
+ * @author      Thuy Dinh Xuan <thuydx@zendgroup.vn>
+ * @link        http://zendgroup.vn
+ * @copyright   Copyright (c) 2012-2013 ZEND GROUP. All rights reserved (http://www.zendgroup.vn)
+ * @license     http://zendgroup.vn/license/
+ * @version     $0.0.1$
+ * Mar 6, 2014
+ *
+ * LICENSE
+ *
+ * This source file is copyrighted by ZEND GROUP, full details in LICENSE.txt.
+ * It is also available through the Internet at this URL:
+ * http://zendgroup.vn/license/
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the Internet, please send an email
+ * to license@zendgroup.vn so we can send you a copy immediately.
+ *
  */
 
 namespace SyntaxHighlight;
